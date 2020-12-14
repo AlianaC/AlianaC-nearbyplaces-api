48615bf818b79f5d7065b6b63cadccedff59015c
AlianaC <alianacampas@email.arizona.edu>
2020-12-14 16:44:37 -0700

    added sql and server functions

---
+++ db.js
@@ -0,0 +1,59 @@
+'use strict';
+
+require('dotenv').config();
+const { Pool } = require('pg');
+
+const postgreConnectionString =
+    `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DATABASE}`;
+
+console.log(postgreConnectionString);
+
+const postgrePool = new Pool({
+    connectionString: process.env.DATABASE_URL ? process.env.DATABASE_URL : postgreConnectionString,
+    ssl: { rejectUnauthorized: false }
+});
+
+function getPlaces() {
+    return postgrePool.query("SELECT * FROM nearbyplaces.places;").then(x => x.rows);
+};
+
+function getReviews(id) {
+    return postgrePool.query("SELECT * FROM nearbyplaces.reviews WHERE placeid = $1;", [id]).then(x => x.rows);
+}
+
+function search(searchTerm, city, state) {
+    let query = `SELECT * FROM nearbyplaces.places`;
+    let conditions = [];
+    if(searchTerm){
+        conditions.push(`upper(cat) LIKE upper('${searchTerm}')`);
+    }
+    if(city){
+        conditions.push(`upper(city) LIKE upper('${city}')`);
+    }
+    if(state){
+        conditions.push(`upper(state) LIKE upper('${state}')`);
+    }
+
+    if(conditions.length != 0){
+        query += " WHERE ";
+        query += conditions.join(" AND ");
+    }
+    
+    console.log(query);
+
+    return postgrePool.query(`${query};`).then(x => x.rows);
+}
+
+function addPlace(name, city, state, cat, description) {
+    let query = `INSERT INTO nearbyplaces.places ("name", city, state, cat, description) VALUES('${name}', '${city}', '${state}', '${cat}', '${description}');`
+    return postgrePool.query(query).then(x => x.rows);
+}
+
+function addReview(author, rate, rating, placeid){
+    let query = `INSERT INTO nearbyplaces.reviews (author, rate, rating, placeid) VALUES('${author}', ${rate}, '${rating}', ${placeid});`;
+    return postgrePool.query(query).then(x => x.rows);
+}
+    
+
+module.exports = { getPlaces, getReviews, addPlace, search, addReview };
+
--- server.js
+++ server.js
@@ -1,15 +1,53 @@
 const express = require('express');
 var cors = require('cors');
+var db = require("./db");
 var bodyParser = require('body-parser');
-
 
 const app = express();
 const port = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(bodyParser.json());
+
 
 app.get('/', (request,response) => {
     response.send('Welcome to nearby places API');
 }); 
 
+app.get('/places', (request,response) => {
+    db.getPlaces().then(x => response.json(x));
+});
+
+app.get('/reviews/:placeid', (request, response) => {
+    let reqID = request.params.placeid;
+    db.getReviews(reqID).then(x => response.json(x));
+});
+
+app.get('/search', (request, response) => {
+    let searchTerm = request.body.searchTerm;
+    let city = request.body.city;
+    let state = request.body.state;
+    db.search(searchTerm, city, state).then(x => response.json(x));
+});
+
+app.post('/place', (request, response) => {
+    console.log(request.body);
+    let name = request.body.name;
+    let city = request.body.city;
+    let state = request.body.state;
+    let cat = request.body.cat;
+    let description = request.body.description;
+    db.addPlace(name, city, state, cat, description).then(x => response.json({message: "Place successfully added."}));
+});
+
+app.post('/review', (request, response) => {
+    console.log(request.body);
+    let author = request.body.author;
+    let rate = request.body.rate;
+    let rating = request.body.rating;
+    let placeid = request.body.placeid;
+    db.addReview(author, rate, rating, placeid).then(x => response.json({message: "Review successfully added."}));
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`);
